27f267c6ba52343b8b815f86c28c5e329464dca3
Srimathi <srimaravi96@gmail.com>
2020-02-23 16:19:08 -0800

    Flappy Fish Game

---
+++ Assets/BgLooper.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BgLooper : MonoBehaviour {
+
+	
+
+	void Start() {
+
+	}
+
+	void OnTriggerEnter2D(Collider2D collider) {
+		Debug.Log ("Triggered: " + collider.name);
+
+		float widthOfBGObject = ((BoxCollider2D)collider).size.x;
+
+		Vector3 pos = collider.transform.position;
+		if(collider.tag == "Pipe") {
+       pos.x += 23.38f;
+		}
+else
+{
+
+		pos.x += 8.1642f;
+
+
+}
+		collider.transform.position = pos;
+
+	}
+}
---
+++ Assets/Score.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+using System.Collections;
+
+public class Score : MonoBehaviour {
+
+	static int score = 0;
+	static int highScore = 0;
+
+	static Score instance;
+
+	static public void AddPoint() {
+		if(instance.fish.dead)
+			return;
+
+		score++;
+
+		if(score > highScore) {
+			highScore = score;
+		}
+	}
+
+	Fish fish;
+
+	void Start() {
+		instance = this;
+		GameObject player_go = GameObject.FindGameObjectWithTag("Player");
+		if(player_go == null) {
+			Debug.LogError("Could not find an object with tag 'Player'.");
+		}
+
+		fish = player_go.GetComponent<Fish>();
+		score = 0;
+		highScore = PlayerPrefs.GetInt("highScore", 0);
+	}
+
+	void OnDestroy() {
+		instance = null;
+		PlayerPrefs.SetInt("highScore", highScore);
+	}
+
+	void Update () {
+	
+transform.GetComponentInChildren<Text>().text = "Score: " + score + "\nHigh Score: " + highScore;
+
+
+	}
+}
+
+
